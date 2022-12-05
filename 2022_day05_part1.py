559adc3d850183d7c317c7ecd1cdc23664664028
paulgfoster <paulgfoster@yahoo.co.uk>
2022-12-05 22:58:15 +0800

    Day 05

--- 2022_day05_part1.py
+++ 2022_day05_part1.py
@@ -101,19 +101,6 @@ with open('2022_day05_instructions.txt') as csvfile:
         crate = ' '
    
         #get current crate and pop off stack and do it qty times ...
-
-        '''
-        move 1 from 2 to 1
-        move 3 from 1 to 3
-        move 2 from 2 to 1
-        move 1 from 1 to 2
-
-            [D]    
-        [N] [C]    
-        [Z] [M] [P]
-         1   2   3 
-        '''
-
         while True:
 
             if qty == 0:
